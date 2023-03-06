9c98b16c2f94c025ec66c4c8faf811113ce05eea
viansipo <viansipo@gmail.com>
2023-03-06 20:48:16 -0300

    mejorando recibimiento

--- docs/js/bienvenida.js
+++ docs/js/bienvenida.js
@@ -1,1 +1,3 @@
-window.alert("¡Bienvenidos a mi Curriculum Vitae! \n Welcome to my Curriculum Vitae! \n Bem-vindo ao meu Curriculum Vitae! \n 欢迎来到我的简历！")
+Swal.fire({
+    title: "¡Bienvenidos a mi Curriculum Vitae! \n Welcome to my Curriculum Vitae! \n Bem-vindo ao meu Curriculum Vitae! \n 欢迎来到我的简历！"
+});
